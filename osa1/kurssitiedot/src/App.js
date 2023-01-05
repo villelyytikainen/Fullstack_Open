40d3f5a835575ab8432104a68fa1dfbb5969791f
Ville Lyytikäinen <corpsefin@gmail.com>
2023-01-05 16:46:16 +0200

    Jatkettu tehtävää eteenpäin..

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,27 +1,30 @@
 
 
 function App() {
-    const course = 'Half Stack application development';
-    const parts = [
-        {
-            part: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            part: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            part: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course =
+    {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                part: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                part: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                part: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 }
